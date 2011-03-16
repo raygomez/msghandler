e1daf8570e49128184e68c806600b5acbc4e8c58
rayvincent.gomez <rayvincent.gomez@gmail.com>
2011-03-16 12:31:20 +0000

    Added all the models used. Create a simple homepage. Added a simple authentication 	username:randy@gmail.com 	password:1 Added a lot of functions under default

--- controllers/default.py
+++ controllers/default.py
@@ -9,14 +9,52 @@
 ## - call exposes all registered services (none by default)
 #########################################################################
 
+@auth.requires_login()
 def index():
     """
     example action using the internationalization operator T and flash
     rendered by views/default/index.html or views/generic.html
     """
-    response.flash = T('You are successfully running web2py.')
-    return dict(message=T('Hello World'))
+    #groups = db(db.auth_membership.user_id == auth.user_id).select(db.auth_membership.group_id)
+    groups = db(db.auth_membership.user_id == auth.user_id).select()
+    roles = []
+    for group in groups:
+        roles.append(group.group_id.role)
 
+    messages = db().select(db.msg.id, db.msg.subject, db.msg.created_by, orderby=db.msg.subject)
+    contacts = db().select(db.contact.id, db.contact.name, orderby=db.contact.name)
+    tags = db().select(db.tag.id, db.tag.name, orderby=db.tag.name)
+    
+    return dict(grp=roles, messages=messages, contacts=contacts, tags=tags)
+     
+def show_message():
+    this_message = db.msg(request.args(0)) or redirect(URL('index'))
+    
+    attachments = db(db.msg_attachment.msg_id == this_message.id).select(orderby=db.msg_attachment.attach_time)
+    
+    form = crud.update(db.msg, this_message, next=URL('index'))
+    return dict(form=form, attachments=attachments,id=this_message.id)
+
+
+def show_contact():
+    this_contact = db.contact(request.args(0)) or redirect(URL('index'))
+    form = crud.update(db.contact, this_contact, next=URL('index'))
+    return dict(form=form)
+
+def show_tag():
+    tag = db.tag(request.args(0)) or redirect(URL('index'))
+    form = crud.update(db.tag, tag, next=URL('index'))
+    return dict(form=form)
+
+
+def data():
+    return dict(form=crud())
+
+def delete_attach():    
+    session.flash = T('Attachment successfully deleted.')
+    db(db.msg_attachment.id==request.args(0)).delete()
+    redirect(URL(f='show_message', args=request.args(1)))
+    
 def user():
     """
     exposes:
@@ -41,7 +79,38 @@ def download():
     """
     return response.download(request,db)
 
+def create_message():
+    """
+    allows to create a simple message for testing
+    """
+    form = crud.create(db.msg, next=URL('index'))
+    return dict(form = form)
 
+def create_contact():
+    """
+    allows to create a simple contact for testing
+    """
+    form = crud.create(db.contact, next=URL('index'))
+    
+    return dict(form = form)
+
+def create_attachment():
+    """
+    allows to create a simple attachment for testing
+    """
+    db.msg_attachment.msg_id.default = request.args(0)
+    form = crud.create(db.msg_attachment, next=URL('show_message', args=request.args(0)))
+    #db.msg_attachment.msg_id.default = request.arg(0)
+    return dict(form = form)
+
+
+def create_tag():
+    """
+    allows to create a simple contact for testing
+    """
+    form = crud.create(db.tag, next=URL('index'))
+    return dict(form = form)
+    
 def call():
     """
     exposes services. for example:
@@ -51,5 +120,3 @@ def call():
     """
     session.forget()
     return service()
-
-
