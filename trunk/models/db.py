bcf4b5516e3b87ead850cf128fac9a7510e6d1df
rayvincent.gomez@gmail.com <rayvincent.gomez@gmail.com@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-03-21 16:56:17 +0000

    Disabled registration module. Change the delete button to an image for both show and create message Do away with some ununsed variables.

--- trunk/models/db.py
+++ trunk/models/db.py
@@ -46,6 +46,7 @@ auth.settings.registration_requires_verification = True
 auth.settings.registration_requires_approval = True
 auth.messages.verify_email = 'Click on the link http://'+request.env.http_host+URL('default','user',args=['verify_email'])+'/%(key)s to verify your email'
 auth.settings.reset_password_requires_verification = True
+auth.settings.actions_disabled.append('register') 
 auth.messages.reset_password = 'Click on the link http://'+request.env.http_host+URL('default','user',args=['reset_password'])+'/%(key)s to reset your password'
 
 #########################################################################
