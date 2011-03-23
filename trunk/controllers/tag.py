15cd0f4eae8c3cf93dcff5cf7b0b721d89a59d1b
rgfernandez <rgfernandez@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-03-23 03:14:55 +0000

    Updated messages for tag methods.

--- trunk/controllers/tag.py
+++ trunk/controllers/tag.py
@@ -6,26 +6,26 @@ def index():
 def create():
     form = crud.create(db.tag, next=URL('index'))
     if form.accepts(request.vars, session):
-        response.flash = 'form accepted'
+        response.flash = T('Successfully created %s' % form.vars.name)
     elif form.errors:
-        response.flash = 'form has errors'
-    else:
-        response.flash = 'please fill the form'
+        response.flash = T('Error creating tag')
     return dict(form=form)
 
 def read():
     record = db.tag(request.args(0)) or redirect(URL('index'))
-    form = crud.read(db.tag, record_id=record.id)
+    form = crud.read(db.tag, record=record.id)
     return dict(form=form)
 
 @auth.requires_login()
 def update():
     record = db.tag(request.args(0)) or redirect(URL('create'))
-    form = crud.update(db.tag, record_id=record.id)
+    form = crud.update(db.tag, record=record.id,
+                       message=T('Successfully updated %s' % (record.name)))
     return dict(form=form)
 
 @auth.requires_login()
 def delete():
     record = db.tag(request.args(0)) or redirect(URL('index'))
-    form = crud.delete(db.tag, record_id=request.args(0), next=URL('index'), message='succesful')
+    form = crud.delete(db.tag, record_id=record.id, next=URL('index'),
+                       message=T('Successfully deleted %s' % (record.name)))
     return dict(form=form)
