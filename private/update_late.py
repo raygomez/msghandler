5c885cdd7a4fff4b617bf5358f71ec979988b4ff
rgfernandez <rgfernandez@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-03-29 23:41:36 +0000

    Implementation of insert into table msg_tag

--- private/update_late.py
+++ private/update_late.py
@@ -6,8 +6,7 @@ Run me using
 
 import datetime
 
-#late_id = db(db.tag.name=='Late').select(db.tag.id).as_list()[0].values()
-#print late_id
+late_id = db(db.tag.name=='Late').select(db.tag.id).first().id
 
 #cond_all = db(db.msg_tag.tag_id.belongs(late_id))._select(db.msg.ALL)
 #print cond_all
@@ -21,13 +20,13 @@ filt_id = db(db.msg.parent_msg>0).select(
                 distinct=True
             ).as_list()[0].values()
 
-print [row.id for row in db((db.msg.parent_msg>0)
-              & cond_time).select(
-                groupby=db.msg.parent_msg,
-                orderby=~db.msg.create_time)
-        ]
+[db.msg_tag.insert(msg_id=row.id, tag_id=late_id) for row in
+    db((db.msg.parent_msg>0)
+       & cond_time).select(
+            groupby=db.msg.parent_msg,
+            orderby=~db.msg.create_time)]
 
-print [row.id for row in db((db.msg.parent_msg==0)
-              & ~db.msg.id.belongs(filt_id)
-            ).select()
-        ]
+[db.msg_tag.insert(msg_id=row.id, tag_id=late_id) for row in
+    db((db.msg.parent_msg==0)
+       & ~db.msg.id.belongs(filt_id)
+    ).select()]
