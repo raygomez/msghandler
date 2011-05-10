91d0a2854ef74a29c1a7f4c4b9ef60e0a1351dfd
rayvincent.gomez@gmail.com <rayvincent.gomez@gmail.com@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-05-10 20:06:10 +0000

    Change the link of Groups in the Main Menu. User can now add groups in the group function.

--- trunk/models/menu.py
+++ trunk/models/menu.py
@@ -42,7 +42,7 @@ if auth.is_logged_in():
 
     if auth.has_membership('Admin', auth.user.id) or auth.has_membership('Nurse', auth.user.id):
         response.menu+= [
-            (T('Groups'), False, URL(request.application,'default','index'), []),
+            (T('Groups'), False, URL(request.application,'default','groups'), []),
             (T('Tags'), False, URL(request.application,'default','index'), []),
             (T('Contacts'), False, URL(request.application,'default','index'), []),
             (T('Help'), False, URL(request.application,'default','index'), []),    
