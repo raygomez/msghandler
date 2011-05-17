f6d0a93eacb8ee7e96d0a6fe146f7e903ad1147b
rgfernandez <rgfernandez@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-05-17 14:25:57 +0000

    basic, yet inefficient query. does not tag messages already tagged as 'late'. tags only parent messages.

--- private/update_late.py
+++ private/update_late.py
@@ -15,17 +15,37 @@ late_id = db(db.tag.name=='Late').select(db.tag.id).first().id
 cond_time = (db.msg.create_time <
                 (datetime.datetime.now()-datetime.timedelta(minutes=5))
             )
-filt_id = db(db.msg.parent_msg>0).select(
-                db.msg.parent_msg,
-                distinct=True
-            ).as_list()[0].values()
 
-[db.msg_tag.insert(msg_id=row.id, tag_id=late_id) for row in
-    db((db.msg.parent_msg>0)
-       & cond_time
-    ).select(db.msg.id, groupby=db.msg.parent_msg)]
+qry = []
 
-[db.msg_tag.insert(msg_id=row.id, tag_id=late_id) for row in
-    db((db.msg.parent_msg==0)
-       & ~db.msg.id.belongs(filt_id)
-    ).select(db.msg.id)]
+# list of child comments
+# group by parent message
+# get to be tagged as late
+qry1 = db((db.msg.parent_msg>0) & cond_time).select(
+                db.msg.parent_msg, db.msg.id, orderby=~db.msg.create_time,
+                groupby=db.msg.parent_msg
+            ).as_list()
+for elem in qry1:
+    qry.append(elem['parent_msg'])
+
+# list messages without comments
+# get to be tagged as late
+qry2 = db((db.msg.parent_msg==0) & cond_time).select(
+                db.msg.id, orderby=~db.msg.create_time
+            ).as_list()
+for elem in qry2:
+    x = elem['id']
+    if x not in qry:
+        qry.append(x)
+    
+# get already tagged as late
+# remove from qry
+qry3 = db().select(db.msg_tag.msg_id).as_list()
+for elem in qry3:
+    x = elem['msg_id']
+    if x in qry:
+        qry.remove(x)
+
+[db.msg_tag.insert(msg_id=elem, tag_id=late_id) for elem in qry]
+
+db.commit()
