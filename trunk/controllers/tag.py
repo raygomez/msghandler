68b0e4b6ce721c3d94fdfcc80e1a38a71aa37f0d
rayvincent.gomez@gmail.com <rayvincent.gomez@gmail.com@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-06-09 09:57:38 +0000

    Moved the add_tag from default/index to tag/index

--- trunk/controllers/tag.py
+++ trunk/controllers/tag.py
@@ -1,5 +1,19 @@
+dbutils = local_import('utils.dbutils')
+
 @auth.requires(auth.has_membership('Admin')
                or auth.has_membership('Telehealth'))
 def index():
     tags = db(db.tag.id).select(orderby=~db.tag.id)
     return dict(tags=tags)
+
+@auth.requires(auth.has_membership('Admin')
+               or auth.has_membership('Telehealth'))
+def add_tag():
+    tags = db(db.tag.name == request.vars.name).select()
+    if len(tags) == 0:
+        id = db.tag.insert(**request.vars)
+        dbutils.log_event(db, user_id=auth.user.id, item_id=id,
+                          table_name='tag', access='create',
+                          details=request.vars.name)
+        return `id`
+    else: return '0'
