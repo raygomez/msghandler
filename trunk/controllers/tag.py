f5755e212025177582e6bf009d32d2dbd19ecc76
rayvincent.gomez@gmail.com <rayvincent.gomez@gmail.com@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-06-09 09:33:18 +0000

    Redirected Tag menu to tag.py Clear previous content of tag.py Copied default/tags.html to tags/index.html

--- trunk/controllers/tag.py
+++ trunk/controllers/tag.py
@@ -1,31 +1,5 @@
+@auth.requires(auth.has_membership('Admin')
+               or auth.has_membership('Telehealth'))
 def index():
-    form = crud.select(db.tag, orderby=db.tag.name)
-    return dict(form=form)
-
-@auth.requires_login()
-def create():
-    form = crud.create(db.tag, next=URL('index'))
-    if form.accepts(request.vars, session):
-        response.flash = T('Successfully created %s' % form.vars.name)
-    elif form.errors:
-        response.flash = T('Error creating tag')
-    return dict(form=form)
-
-def read():
-    record = db.tag(request.args(0)) or redirect(URL('index'))
-    form = crud.read(db.tag, record=record.id)
-    return dict(form=form)
-
-@auth.requires_login()
-def update():
-    record = db.tag(request.args(0)) or redirect(URL('create'))
-    form = crud.update(db.tag, record=record.id,
-                       message=T('Successfully updated %s' % (record.name)))
-    return dict(form=form)
-
-@auth.requires_login()
-def delete():
-    record = db.tag(request.args(0)) or redirect(URL('index'))
-    form = crud.delete(db.tag, record_id=record.id, next=URL('index'),
-                       message=T('Successfully deleted %s' % (record.name)))
-    return dict(form=form)
+    tags = db(db.tag.id).select(orderby=~db.tag.id)
+    return dict(tags=tags)
