657686b018eb1d7d1bf4bacbb81b9e6fa96f80ac
rayvincent.gomez <rayvincent.gomez@gmail.com>
2011-06-17 23:53:34 +0000

    Rename update_contact to update and moved to contacts.py. Contacts can now be updated.

--- controllers/contacts.py
+++ controllers/contacts.py
@@ -1,5 +1,29 @@
 dbutils = local_import('utils.dbutils')
 
+@auth.requires(auth.has_membership('Admin')
+               or auth.has_membership('Telehealth'))
+def update():
+    id = request.vars.id
+    contact_type = request.vars.contact_type
+    contact_info = request.vars.contact_info
+    
+    contact = db.contact[id]    
+    
+    details = []
+    if contact_type != contact.contact_type:
+        details.append('contact type changed from ' + contact.contact_type
+                       + ' to ' + contact_type)
+    if contact_info != contact.contact_info:
+        details.append('contact info changed from ' + contact.contact_info
+                       + ' to ' + contact_info)
+    details = ', '.join(details)
+    
+    dbutils.log_event(db, details=details, user_id=auth.user.id, item_id=id,
+                      table_name='contact', access='update')
+    db.contact[id] = dict(contact_type=contact_type, contact_info=contact_info)
+    return ''
+
+
 @auth.requires_membership('Admin')
 def delete():
     contact = db.contact[request.vars.id]
