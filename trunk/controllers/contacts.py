e9147bff2633e08d7a3583b9705f9e9aeb5fa869
rayvincent.gomez@gmail.com <rayvincent.gomez@gmail.com@8998ff36-7d3b-fa34-ccb0-0598aa3a0193>
2011-06-18 04:42:48 +0000

    Removed Contacts in menu.py Removed contact.name in logs when deleting contacts. Change display of contact type from input to select.

--- trunk/controllers/contacts.py
+++ trunk/controllers/contacts.py
@@ -39,11 +39,10 @@ def update():
 @auth.requires_membership('Admin')
 def delete():
     contact = db.contact[request.vars.id]
-    name = contact.name
     contact_type = contact.contact_type
     contact_info = contact.contact_info
     dbutils.log_event(db, user_id=auth.user.id,
                       item_id=request.vars.id, table_name='contact',
-                      access='delete', details=','.join([name,contact_type,contact_info]))
+                      access='delete', details=','.join([contact_type,contact_info]))
     del db.contact[request.vars.id]
     return ''
