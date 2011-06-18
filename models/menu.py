877d0aacdfd180cc92d545634b3ef57fe8017a97
rayvincent.gomez <rayvincent.gomez@gmail.com>
2011-06-18 04:42:48 +0000

    Removed Contacts in menu.py Removed contact.name in logs when deleting contacts. Change display of contact type from input to select.

--- models/menu.py
+++ models/menu.py
@@ -45,7 +45,6 @@ if auth.is_logged_in():
     if auth.has_membership('Admin', auth.user.id) or auth.has_membership('Telehealth', auth.user.id):
         response.menu+= [
             (T('Tags'), False, URL(request.application,'tags','index'), []),
-            (T('Contacts'), False, URL(request.application,'default','contacts'), []),
             (T('Help'), False, URL(request.application,'default','index'), []),    
         ]
 
